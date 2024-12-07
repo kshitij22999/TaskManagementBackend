7555b71a66b0f00dd1740cedaeee84e6646e2034
kshitij <kshitij22999@gmail.com>
2024-12-08 01:34:07 +0530

    fixed cors issue

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, session
 from routes.auth_routes import auth
 from routes.task_routes import tasks_bp
+from flask_cors import CORS
 
 app = Flask(__name__)
 
@@ -8,6 +9,8 @@ app.register_blueprint(auth, url_prefix='/auth')
 app.register_blueprint(tasks_bp, url_prefix='/api')
 
 app.secret_key = "kj12b3i124b0dnas"
+
+CORS(app)
 
 if __name__ == "__main__":
     app.run(debug=True)
